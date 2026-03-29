17827468df1f9433501809681f792e732185460f
dacanayliezelann-afk <dacanayliezelann@gmail.com>
2026-03-29 11:16:15 +0800

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -31,3 +31,6 @@ const PORT=process.env.PORT||5000;
 app.listen(PORT,()=>{
 console.log(`Serverisrunningonhttp://localhost:${PORT}`);
 });
+app.get('/', (req, res) => {
+  res.send('Server is running!');
+});
