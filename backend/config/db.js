4d01e675ee5f38a8be68d8a290108c4d9c029b2e
dacanayliezelann-afk <dacanayliezelann@gmail.com>
2026-03-29 11:35:52 +0800

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,11 +1,13 @@
 const mongoose = require('mongoose');
-const   connectDB=async()=>{
-  try{
-    const conn = await mongoose.connect(process.env.MONGO_URI);
-    console.log(`MongoDBConnected:${conn.connection.host}`);
-}catch(error){
-    console.error('Databaseconnection error:', error.message);
-    process.exit(1); //Stopthe server if DB fails
-}
+
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGODB_URI); 
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.error(`Database connection error: ${error.message}`);
+    process.exit(1);
+  }
 };
-module.exports=connectDB;
+
+module.exports = connectDB;;
